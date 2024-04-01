5f8317e61945b2bf6dd1e3dbcc5a3cf2cba6509b
Apoorv Sohal <apoorvsohal@protonmail.com>
2024-04-01 23:55:20 +0530

    feat: added func to check block type

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,13 @@
+import re
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
@@ -8,3 +18,35 @@ def markdown_to_blocks(markdown):
         filtered_blocks.append(block)
 
     return filtered_blocks
+
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return block_type_heading
+
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return block_type_code
+
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return block_type_paragraph
+        return block_type_quote
+
+    if block.startswith(("* ", "- ")):
+        for line in lines:
+            if not line.startswith(("* ", "- ")):
+                return block_type_paragraph
+        return block_type_unordered_list
+
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return block_type_paragraph
+            i += 1
+        return block_type_ordered_list
+
+    return block_type_paragraph
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,14 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import (
+    markdown_to_blocks,
+    block_to_block_type,
+    block_type_ordered_list,
+    block_type_code,
+    block_type_unordered_list,
+    block_type_quote,
+    block_type_heading,
+    block_type_paragraph,
+)
 
 
 class TestMarkdownToHTML(unittest.TestCase):
@@ -45,6 +54,22 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ],
         )
+
+    def test_block_to_block_type(self):
+        markdown = "1. Note 1\n2. Note 2"
+        self.assertEqual(block_type_ordered_list, block_to_block_type(markdown))
+        markdown = "* Note 1\n* Note 2"
+        self.assertEqual(block_type_unordered_list, block_to_block_type(markdown))
+        markdown = "- Note 1\n- Note 2"
+        self.assertEqual(block_type_unordered_list, block_to_block_type(markdown))
+        markdown = "```\ncode\n````"
+        self.assertEqual(block_type_code, block_to_block_type(markdown))
+        markdown = ">Quote\n>more quotes"
+        self.assertEqual(block_type_quote, block_to_block_type(markdown))
+        markdown = "paragraph"
+        self.assertEqual(block_type_paragraph, block_to_block_type(markdown))
+        markdown = "# Heading"
+        self.assertEqual(block_type_heading, block_to_block_type(markdown))
 
 
 if __name__ == "__main__":
